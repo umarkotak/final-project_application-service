14985ed5068fa19c5b44ba8cc90c824e5996a6d3
umarkotak <umarkotak@yahoo.co.id>
2017-12-07 16:05:21 +0700

    final form to start

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171206080009) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "drivers", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
