e42f8d6eafe422a362e32714b39d8db441668160
umarkotak <umarkotak@yahoo.co.id>
2017-12-08 16:03:59 +0700

    working on unit test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,113 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  before :each do
+    @user = create(:user)
+    session[:user_id] = @user.id
+  end
+
+  describe "GET #index" do
+    it "populates an array of all users" do
+      user1 = create(:user, username: "umar1")
+      user2 = create(:user, username: "umar2")
+      
+      get :index
+      expect(assigns(:users)).to eq(User.all)
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested user to @user" do
+      get :show, params: { id: @user }
+      expect(assigns(:user)).to eq(@user)
+    end
+
+    it "renders the :show template" do
+      get :show, params: { id: @user}
+      expect(response).to render_template :show
+    end
+
+    it "redirect to @user if user try to show another user" do
+      user1 = create(:user, username: "umar1")
+      get :show, params: { id: user1 }
+      expect(response).to redirect_to(@user)
+    end
+  end
+
+  describe "GET #new" do
+
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested user to @user" do
+      get :edit, params: { id: @user }
+      expect(assigns(:user)).to eq(@user)
+    end
+
+    it "renders the :edit template" do
+      get :edit, params: { id: @user}
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe "POST #create" do
+    it "assigns 50000 credit to new users" do
+      expect(@user.credit).to eq 50000
+    end
+
+    it "saves the new user in the database" do
+      expect {
+        post :create, params: { user: attributes_for(:user, username: 'umar1') }
+      }.to change(User, :count).by(1)
+    end
+
+    it "is redirected to login page" do
+      post :create, params: { user: attributes_for(:user, username: 'umar1') }
+      expect(response).to redirect_to(login_path)
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with valid attributes" do
+      it "locates the requested user" do
+
+      end
+
+      it "saves the new password" do
+        patch :update, params: { id: @user, user: attributes_for(:user, password: 'newlongpassword', password_confirmation: 'newlongpassword') }
+        @user.reload
+        expect(@user.authenticate('newlongpassword')).to eq(@user)
+      end
+
+      it "redirects to user#index" do
+        patch :update, params: { id: @user, user: attributes_for(:user) }
+        expect(response).to redirect_to(user_path(assigns[:user]))
+      end
+
+      it "disables login with old password" do
+        patch :update, params: { id: @user, user: attributes_for(:user, password: 'newlongpassword', password_confirmation: 'newlongpassword')}
+        @user.reload
+        expect(@user.authenticate('oldpassword')).to eq(false)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not update the user in the database" do
+        patch :update, params: { id: @user, user: attributes_for(:user, password: nil, password_confirmation: nil) }
+        @user.reload
+        expect(@user.authenticate(nil)).to eq(false)
+      end
+
+      it "re-renders the :edit template" do
+        patch :update, params: { id: @user, user: attributes_for(:user, username: nil) }
+        expect(response).to render_template :edit
+      end
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,65 +5,83 @@ RSpec.describe User, type: :model do
     expect(build(:user)).to be_valid
   end
 
-  it "is invalid without username" do
-    user = build(:user, username: nil)
-    user.valid?
-    expect(user.errors[:username]).to include("can't be blank")
+  describe "attributes validation" do
+
+    it "is invalid without username" do
+      user = build(:user, username: nil)
+      user.valid?
+      expect(user.errors[:username]).to include("can't be blank")
+    end
+
+    it "is invalid with duplicate username" do
+      user1 = create(:user, username: "user")
+      user2 = build(:user, username: "user")
+      user2.valid?
+      expect(user2.errors[:username]).to include("has already been taken")
+    end
+
+    it "is invalid without password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "is invalid with password length < 4 characters" do
+      user = build(:user, password: "123")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 4 characters)")
+    end
+
+    it "is invalid with wrong password confirmation" do
+      user = build(:user, password: "password", password_confirmation: "other_password")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "is invalid without full name" do
+      user = build(:user, full_name: nil)
+      user.valid?
+      expect(user.errors[:full_name]).to include("can't be blank")
+    end
+
+    it "is invalid without email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "is invalid with wrong email format" do
+      user = build(:user, email: "wrongEmail")
+      user.valid?
+      expect(user.errors[:email]).to include("email format is invalid")
+    end
+
+    it "is invalid without a phone number" do
+      user = build(:user, phone: nil)
+      user.valid?
+      expect(user.errors[:phone]).to include("can't be blank")
+    end
+
+    it "is invalid with wrong phone format" do
+      user = build(:user, phone: "wrongNumber")
+      user.valid?
+      expect(user.errors[:phone]).to include("is not a number")
+    end
+
   end
 
-  it "is invalid with duplicate username" do
-    user1 = create(:user, username: "user")
-    user2 = build(:user, username: "user")
-    user2.valid?
-    expect(user2.errors[:username]).to include("has already been taken")
-  end
+  describe "top up validation" do
+    it "let the user to topup their credit" do
+      user = create(:user)
+      user.topup("5000")
+      expect(user.credit).to eq(55000)
+    end
 
-  it "is invalid without password" do
-    user = build(:user, password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include("can't be blank")
-  end
-
-  it "is invalid with password length < 4 characters" do
-    user = build(:user, password: "123")
-    user.valid?
-    expect(user.errors[:password]).to include("is too short (minimum is 4 characters)")
-  end
-
-  it "is invalid with wrong password confirmation" do
-    user = build(:user, password: "password", password_confirmation: "other_password")
-    user.valid?
-    expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-  end
-
-  it "is invalid without full name" do
-    user = build(:user, full_name: nil)
-    user.valid?
-    expect(user.errors[:full_name]).to include("can't be blank")
-  end
-
-  it "is invalid without email" do
-    user = build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it "is invalid with wrong email format" do
-    user = build(:user, email: "wrongEmail")
-    user.valid?
-    expect(user.errors[:email]).to include("email format is invalid")
-  end
-
-  it "is invalid without a phone number" do
-    user = build(:user, phone: nil)
-    user.valid?
-    expect(user.errors[:phone]).to include("can't be blank")
-  end
-
-  it "is invalid with wrong phone format" do
-    user = build(:user, phone: "wrongNumber")
-    user.valid?
-    expect(user.errors[:phone]).to include("is not a number")
+    it "will not let the user to topup their credit with invalid value" do
+      user = create(:user)
+      user.topup(nil)
+      expect(user.credit).to eq(50000)
+    end
   end
 
 end
