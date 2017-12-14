5c615b01a7c266904e80756ccf2ac10e096b104d
umarkotak <umarkotak@yahoo.co.id>
2017-12-14 13:41:02 +0700

    async allocating driver done

--- app/consumers/consumer_test_consumer.rb
+++ app/consumers/consumer_test_consumer.rb
@@ -3,22 +3,24 @@ class ConsumerTestConsumer < Racecar::Consumer
 
   def process(message)
     data = eval(message.value)
-    # data = {}
-    # data[:order_id] = 148
-    # data[:service_type] = 'gojek'
 
     drivers = Driver.joins(:driver_locations)
     drivers = drivers.where("driver_locations.status = 'online'")
     drivers = drivers.where("drivers.service_type = '#{data[:service_type]}'")
     driver = drivers.order("RANDOM()").first
 
+    driver_location = DriverLocation.find_by(driver_id: driver.id)
+    driver_location.order_id = data[:order_id]
+    driver_location.status = 'busy'
+    driver_location.save
+
     order = Order.find(data[:order_id])
     order.driver = driver
     order.save
 
     puts "=========================================="
-    puts "ORIGINAL MESSAGE = '#{message.value}'"
-    puts "Received Data = #{data}"
+    puts "ORIGINAL MESSAGE    = '#{message.value}'"
+    puts "CONVERTED DATA      = #{data}"
     puts "YOUR DRIVER WILL BE = #{driver.username}"
     puts "=========================================="
   end
